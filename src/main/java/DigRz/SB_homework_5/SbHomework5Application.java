e2ad6a53ab31676a76d7ec827cd95ba67e4d21cf
Yavor <d69@abv.bg>
2023-03-02 21:37:32 +0200

    test commit

--- src/main/java/DigRz/SB_homework_5/SbHomework5Application.java
+++ src/main/java/DigRz/SB_homework_5/SbHomework5Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbHomework5Application {
 
+	//---------------------------------------------------------------
+
 	public static void main(String[] args) {
 		SpringApplication.run(SbHomework5Application.class, args);
 	}
